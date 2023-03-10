54902945c68a457dbf7079278a0c3e8b5cfafc82
francis3400 <francisuche712@gmail.com>
2023-03-10 08:26:01 -0800

    update components folder

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,7 +1,14 @@
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/bookSlice';
+
 const Book = (book) => {
+  const dispatch = useDispatch();
   const {
-    type, title, author, chapter,
+    type, title, author, chapter, id,
   } = book;
+  const handledelete = (id) => {
+    dispatch(removeBook({ id }));
+  };
   return (
     <div className="theBook">
       <h3 className="bookType">{type}</h3>
@@ -9,7 +16,20 @@ const Book = (book) => {
       <h2 className="bookAuthor">{author}</h2>
       <ul className="bookOptions">
         <li className="option">Comments</li>
-        <li className="option">Remove</li>
+        <li>
+          <button
+            className="option"
+            onClick={() => {
+              handledelete(id);
+            }}
+            onKeyDown={() => {
+              handledelete(id);
+            }}
+            type="button"
+          >
+            Remove
+          </button>
+        </li>
         <li className="option">Edit</li>
       </ul>
       <div className="progress">
