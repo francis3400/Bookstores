7cda4e190ad0c7cd932bea44481f051e08e8dac2
francis3400 <francisuche712@gmail.com>
2023-03-12 01:20:38 -0800

    fixed linters errors and updated styles

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,7 +7,9 @@ function Nav() {
       <h1 className="theHeading">Bookstore CMS</h1>
       <nav className="options">
         <Link to="/">BOOKS</Link>
-        <Link to="Categories">CATEGORIES</Link>
+        <Link className="cate" to="Categories">
+          CATEGORIES
+        </Link>
       </nav>
     </div>
   );
